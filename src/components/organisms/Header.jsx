9521e3d79cf35bcf3a276f05064026097f9f8e98
Benjamin <be.retamalr@duocuc.cl>
2025-10-22 13:14:57 -0300

    Actualiza dependencias y mejora el texto de los botones en el encabezado

--- src/components/organisms/Header.jsx
+++ src/components/organisms/Header.jsx
@@ -7,10 +7,15 @@ export default function Header() {
     <header className="flex justify-between items-center p-4 bg-green-200 shadow-md">
       <Title level="h1">Huerto Hogar</Title>
       <nav className="flex gap-4">
-        <Button>Inicio</Button>
+        <Button>Inicio de Sesión</Button>
         <Button>Tienda</Button>
         <Button>Contacto</Button>
+        <Button>Blog</Button>
+        <Button>Perfil</Button>
+        <Button>Carrito</Button>
+
       </nav>
     </header>
   );
 }
+
